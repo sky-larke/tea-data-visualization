3d5720ee89b19252b70bb644ec6d36d8f3147692
skye <67734042+sky-larke@users.noreply.github.com>
2025-04-01 04:32:35 -0500

    Setup for sveltekit, echarts

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { echarts } from "$lib/echarts";
+    const option = {
+        xAxis: {
+            type: "category",
+            data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        yAxis: {
+            type: "value",
+        },
+        series: [
+            {
+                data: [150, 230, 224, 218, 135, 147, 260],
+                type: "line",
+            },
+        ],
+    };
+</script>
+
+<h1>Tea Data Visualization</h1>
+<div class="container" use:echarts={option}></div>
+
+<slot />
+
+<style>
+    .container {
+        width: 500px;
+        height: 500px;
+    }
+</style>
